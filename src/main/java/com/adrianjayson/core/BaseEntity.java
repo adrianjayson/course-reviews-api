0a68123cb9686324f17d7476961bd7acb840c22c
Adrian <adrianjayson13@gmail.com>
2018-05-12 20:56:59 +0800

    initial commit

---
+++ src/main/java/com/adrianjayson/core/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.adrianjayson.core;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private final Long id;
+
+    @Version
+    private Long version;
+
+    public BaseEntity() {
+        this.id = null;
+    }
+}
---
+++ src/main/java/com/adrianjayson/core/DatabaseLoader.java
@@ -0,0 +1,56 @@
+package com.adrianjayson.core;
+
+import com.adrianjayson.course.Course;
+import com.adrianjayson.course.CourseRepository;
+import com.adrianjayson.review.Review;
+import com.adrianjayson.user.User;
+import com.adrianjayson.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class DatabaseLoader implements ApplicationRunner {
+    private final CourseRepository COURSES;
+    private final UserRepository users;
+
+    @Autowired
+    public DatabaseLoader(CourseRepository COURSES, UserRepository users) {
+        this.COURSES = COURSES;
+        this.users = users;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        List<User> students = Arrays.asList(
+                new User("jacobproffer", "Jacob",  "Proffer", "password", new String[] {"ROLE_USER"}),
+                new User("mlnorman", "Mike",  "Norman", "password", new String[] {"ROLE_USER"}),
+                new User("k_freemansmith", "Karen",  "Freeman-Smith", "password", new String[] {"ROLE_USER"}),
+                new User("seth_lk", "Seth",  "Kroger", "password", new String[] {"ROLE_USER"}),
+                new User("mrstreetgrid", "Java",  "Vince", "password", new String[] {"ROLE_USER"}),
+                new User("anthonymikhail", "Tony",  "Mikhail", "password", new String[] {"ROLE_USER"}),
+                new User("boog690", "AJ",  "Teacher", "password", new String[] {"ROLE_USER"}),
+                new User("faelor", "Erik",  "Faelor Shafer", "password", new String[] {"ROLE_USER"}),
+                new User("christophernowack", "Christopher",  "Nowack", "password", new String[] {"ROLE_USER"}),
+                new User("calebkleveter", "Caleb",  "Kleveter", "password", new String[] {"ROLE_USER"}),
+                new User("richdonellan", "Rich",  "Donnellan", "password", new String[] {"ROLE_USER"}),
+                new User("albertqerimi", "Albert",  "Qerimi", "password", new String[] {"ROLE_USER"})
+        );
+
+        users.save(students);
+        users.save(new User("adrianjayson13", "Adrian", "Catambay", "adrianImmortal", new String[]{"ROLE_USER", "ROLE_ADMIN"}));
+
+        Course course1 = new Course("Java Basics", "https://teamtreehouse.com/library/java-basics");
+        course1.addReview(new Review(5, "This course is great!", students.get(2)));
+        course1.addReview(new Review(3, "Alright but lacks detailed explanation.", students.get(5)));
+        COURSES.save(course1);
+
+        Course course2 = new Course("Hibernate Basics", "https://teamtreehouse.com/library/hibernate-basics");
+        course2.addReview(new Review(5, "This course is excellent!", students.get(6)));
+        COURSES.save(course2);
+    }
+}
